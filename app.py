7b2160e83bdaa413441d5e6af78d2356519a5081
milix1953 <79617515+milix1953@users.noreply.github.com>
2023-10-24 08:06:09 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,139 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+
+#======這裡是呼叫的檔案內容=====
+from message import *
+from mongodb_function import *
+#======這裡是呼叫的檔案內容=====
+
+#======python的函數庫==========
+import  os
+#======python的函數庫==========
+
+app = Flask(__name__)
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+# Channel Access Token
+line_bot_api = LineBotApi('wNeecpSbD4LVSh7qG+7lP5dKy4ws9S/BxvNU5bgDn4ufCtkK6ipJzH6xE8XwTJkXLsKKGmI7cz+TtbCctuxMiUUTvCCD0w1s1rpWA3OV0h0Q0SoKKUNkHapkPH2xWho2WaAEuWLVfhUhT6I8g6IpvwdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
+handler = WebhookHandler('e9ec4cc1141ece77dd6173d82037195c')
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    write_one_data(eval(body.replace('false','False')))
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg = event.message.text
+    if '菜單' in msg:
+        message = Carousel_Template()
+        line_bot_api.reply_message(event.reply_token, message)
+    
+    elif '查看我的菜單' in msg:
+        user_id = event.source.user_id
+        menu_items = get_user_menu(user_id)
+
+        if menu_items:
+            menu_text = '\n'.join(menu_items)
+            message = TextSendMessage(text=f"你的菜單如下：\n{menu_text}")
+        else:
+            message = TextSendMessage(text="你還沒有新增任何菜單項目。")
+
+        line_bot_api.reply_message(event.reply_token, message)
+
+    #======MongoDB操作範例======
+
+    elif '@讀取' in msg:
+        datas = read_many_datas()
+        datas_len = len(datas)
+        message = TextSendMessage(text=f'資料數量，一共{datas_len}條')
+        line_bot_api.reply_message(event.reply_token, message)
+
+    elif '@查詢' in msg:
+        datas = col_find('events')
+        message = TextSendMessage(text=str(datas))
+        line_bot_api.reply_message(event.reply_token, message)
+
+    elif '@對話紀錄' in msg:
+        datas = read_chat_records()
+        print(type(datas))
+        n = 0
+        text_list = []
+        for data in datas:
+            if '@' in data:
+                continue
+            else:
+                text_list.append(data)
+            n+=1
+        data_text = '\n'.join(text_list)
+        message = TextSendMessage(text=data_text[:5000])
+        line_bot_api.reply_message(event.reply_token, message)
+
+    elif '@刪除' in msg:
+        text = delete_all_data()
+        message = TextSendMessage(text=text)
+        line_bot_api.reply_message(event.reply_token, message)
+
+    #======MongoDB操作範例======
+
+    else:
+        message = TextSendMessage(text=msg)
+        line_bot_api.reply_message(event.reply_token, message)
+
+# 處理 Postback 事件
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    data = event.postback.data
+    user_id = event.source.user_id
+    
+    if "新增菜單" in data:
+        # 解析資料，獲取要新增的菜單項目資訊
+        menu_item_data = data.split(":")[1]
+        
+        # 使用之前提供的函數進行MongoDB操作
+        add_menu_item(user_id, menu_item_data)
+
+        # 向使用者發送確認訊息
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=f"{menu_item_data} 已成功新增到菜單。")
+        )
+    
+    elif "刪除菜單" in data:
+        # 解析資料，獲取要刪除的菜單項目資訊
+        menu_item_data = data.split(":")[1]
+
+        # 使用之前提供的函數進行MongoDB操作
+        delete_menu_item(user_id, menu_item_data)
+        
+        # 向使用者發送確認訊息
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=f"{menu_item_data} 已成功從菜單中刪除。")
+        )
+    
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
---
+++ mongodb_function.py
@@ -0,0 +1,110 @@
+import pymongo
+
+# 要獲得mongodb網址，請至mongodb網站申請帳號進行資料庫建立，網址　https://www.mongodb.com/
+# 獲取的網址方法之範例如圖： https://i.imgur.com/HLCk99r.png
+client = pymongo.MongoClient("mongodb+srv://40941125:madness3751@cluster0.qnilhoj.mongodb.net/?retryWrites=true&w=majority")
+
+#第一個db的建立
+db = client['MongoClient']
+col = db['Database']
+
+# 為用戶菜單創建一個新的collection
+menu_col = db['UserMenu']
+
+#判斷key是否在指定的dictionary當中，若有則return True
+def dicMemberCheck(key, dicObj):
+    if key in dicObj:
+        return True
+    else:
+        return False
+
+#寫入資料data是dictionary
+def write_one_data(data):
+    col.insert_one(data)
+
+#寫入多筆資料，data是一個由dictionary組成的list
+def write_many_datas(data):
+    col.insert_many(data)
+
+#讀取所有LINE的webhook event紀錄資料
+def read_many_datas():
+    data_list = []
+    for data in col.find():
+        data_list.append(str(data))
+
+    print(data_list)
+    return data_list
+
+#讀取LINE的對話紀錄資料
+def read_chat_records():
+    data_list = []
+    for data in col.find():
+        if dicMemberCheck('events',data):
+            if dicMemberCheck('message',data['events'][0]):
+                if dicMemberCheck('text',data['events'][0]['message']):
+                    print(data['events'][0]['message']['text'])
+                    data_list.append(data['events'][0]['message']['text'])
+        else:
+            print('非LINE訊息',data)
+
+    print(data_list)
+    return data_list
+
+#刪除所有資料
+def delete_all_data():
+    data_list = []
+    for x in col.find():
+        data_list.append(x)   
+
+    datas_len = len(data_list)
+
+    col.delete_many({})
+
+    if len(data_list)!=0:
+        return f"資料刪除完畢，共{datas_len}筆"
+    else:
+        return "資料刪除出錯"
+
+#找到最新的一筆資料
+def col_find(key):
+    for data in col.find({}).sort('_id',-1):
+        if dicMemberCheck(key,data):
+            data = data[key]
+            break
+    print(data)
+    return data
+
+# 添加一個新的菜單項目
+def add_menu_item(user_id, menu_item):
+    menu_data = menu_col.find_one({'user_id': user_id})
+    if menu_data:
+        # 如果用戶已經有菜單，則添加新的菜單項目
+        if 'menu_items' not in menu_data:
+            menu_data['menu_items'] = []
+        if menu_item not in menu_data['menu_items']:
+            menu_data['menu_items'].append(menu_item)
+            menu_col.update_one({'user_id': user_id}, {'$set': {'menu_items': menu_data['menu_items']}})
+    else:
+        # 如果用戶還沒有菜單，則創建一個新的菜單
+        menu_data = {
+            'user_id': user_id,
+            'menu_items': [menu_item]
+        }
+        menu_col.insert_one(menu_data)
+
+# 刪除一個菜單項目
+def delete_menu_item(user_id, menu_item):
+    menu_data = menu_col.find_one({'user_id': user_id})
+    if menu_data and 'menu_items' in menu_data and menu_item in menu_data['menu_items']:
+        menu_data['menu_items'].remove(menu_item)
+        menu_col.update_one({'user_id': user_id}, {'$set': {'menu_items': menu_data['menu_items']}})
+
+# 獲取用戶的菜單
+def get_user_menu(user_id):
+    menu_data = menu_col.find_one({'user_id': user_id})
+    if menu_data and 'menu_items' in menu_data:
+        return menu_data['menu_items']
+    return []
+
+if __name__ == '__main__':
+    print(read_many_datas())
